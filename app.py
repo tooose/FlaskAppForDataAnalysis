b9a1cde9a548fb841d8af1d3e2310d20d211ef7d
Todorche <tose.f5@gmail.com>
2024-01-30 22:58:21 +0100

    preimenuvan APP i voved

---
+++ app.py
@@ -0,0 +1,19 @@
+# Named APP.
+
+# potrebno e da:
+# instalacija na 
+# pymongo
+# sqlalchemy
+# requests
+# json
+
+# API Endpoints
+
+# 1. Вкупна потрошувачка за одреден купувач
+# - Endpoint: /average_spending_by_age
+# - Метод: GET
+# - Опис: Retrieves the total spending for a specific user based on their user ID
+# - Параметри: user_id (integer): Уникатен user ID за купувач
+# - Response: JSON објект кој ги содржи user ID и вкупната потрошувачка за
+# одреден купувач
+
