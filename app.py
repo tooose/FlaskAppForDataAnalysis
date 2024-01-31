cc279ec58c1801c503be059f23a517a5c856c7a0
Todorche <tose.f5@gmail.com>
2024-01-31 20:24:18 +0100

    zgreska

--- app.py
+++ app.py
@@ -7,13 +7,5 @@
 # requests
 # json
 
-# API Endpoints
 
-# 1. Вкупна потрошувачка за одреден купувач
-# - Endpoint: /average_spending_by_age
-# - Метод: GET
-# - Опис: Retrieves the total spending for a specific user based on their user ID
-# - Параметри: user_id (integer): Уникатен user ID за купувач
-# - Response: JSON објект кој ги содржи user ID и вкупната потрошувачка за
-# одреден купувач
 
