7dca9d41738939a6c4bc3a331cb39018a5ff7e4a
Todorche <tose.f5@gmail.com>
2024-02-12 20:22:23 +0100

    api 2

--- app.py
+++ app.py
@@ -30,3 +30,41 @@ def totalSpendByUser(user_id):
     else:
          return jsonify({'error': 'User not found'}), 404
 
+
+@app.route('/average_spending_by_age/', methods=['GET'])
+def averageSpendingByAgeRange():
+    
+    db_path = 'C:\\Users\\Juzer\\Desktop\\FlaskApp\\users_vouchers.db'
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+
+    cursor.execute('''
+    SELECT
+    AVG(CASE WHEN age >= 18 AND age < 24 THEN us.money_spent END) AS avg_money_spent_18_23,
+    AVG(CASE WHEN age >= 25 AND age < 30 THEN us.money_spent END) AS avg_money_spent_25_29,
+    AVG(CASE WHEN age >= 31 AND age < 36 THEN us.money_spent END) AS avg_money_spent_31_36,
+    AVG(CASE WHEN age >= 37 AND age < 47 THEN us.money_spent END) AS avg_money_spent_37_46,
+    AVG(CASE WHEN age >= 48 AND age < 200 THEN us.money_spent END) AS avg_money_spent_48_199
+    FROM user_spending us
+    JOIN user_info ui ON us.user_id = ui.user_id;
+                   ''')
+    
+    result = cursor.fetchone()
+
+    cursor.close()
+    connection.close()
+
+    if result:
+        user_info = {
+            'age_18_24': result[0],
+            'age_25_30': result[1],
+            'age_31_36': result[2],
+            'age_37_46': result[3],
+            'age_47': result[4]
+        }
+        return jsonify(user_info)
+    else:
+         return jsonify({'error': 'No Data found'}), 404
+    
+
+    
