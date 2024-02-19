78149d0c02cabd7e95e188c8c6e6a87ef6315e82
Todorche <tose.f5@gmail.com>
2024-02-19 20:00:53 +0100

    api 3

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # Named APP.
 from flask import Flask, json, jsonify, request
-
+import pymongo
+from pymongo import MongoClient
 import sqlite3
 
 app = Flask(__name__)
+
+
+
 
 @app.route('/total_spend/<int:user_id>', methods=['GET'])
 def totalSpendByUser(user_id):
@@ -33,7 +37,7 @@ def totalSpendByUser(user_id):
 
 @app.route('/average_spending_by_age/', methods=['GET'])
 def averageSpendingByAgeRange():
-    
+
     db_path = 'C:\\Users\\Juzer\\Desktop\\FlaskApp\\users_vouchers.db'
     connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
@@ -65,6 +69,22 @@ def averageSpendingByAgeRange():
         return jsonify(user_info)
     else:
          return jsonify({'error': 'No Data found'}), 404
-    
 
+@app.route('/write_to_mongodb/', methods=['POST'])
+def write_to_mongodb():
+    client = MongoClient()
+    db=client.test_db
+    myCollection = db.test_kolekcija
+    data = request.get_json()
+    posts = db.posts
     
+      # Validate the input JSON
+    if 'user_id' not in data or 'total_spending' not in data:
+        return jsonify({'error': 'Invalid input JSON'}), 400
+    
+    # post_id = posts.insert_one({'user_id': user_id, 'total_spending': total_spending}).inserted_id
+    try:
+        posts.insert_one(data)
+        return jsonify({'message': 'Data written to MongoDB successfully.'}), 201
+    except Exception as e:
+        return jsonify({'message': f'Error writing to MongoDB: {str(e)}'}), 500
